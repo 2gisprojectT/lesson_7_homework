47991025e6a315cf065953dc01a0f93e541f0656
грязный Аркадий < >
2015-12-16 17:37:57 +0600

    Homework 7

--- TestCases.py
+++ TestCases.py
@@ -1,33 +1,15 @@
+# -*- coding:UTF-8 -*-
+from unittest import TestCase
 import unittest
 from selenium import webdriver
-from selenium.webdriver import ActionChains
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
+from page import Page
 
 
-class FirstTestCase(unittest.TestCase):
+class SeleniumTest(TestCase):
     """
     Test Cases:
     Проверка фильтров организаций
     """
-
-    def slide_move(self, hour, slider):
-        """
-        :param hour:  час, на который нужно передвинуть слайдер
-        :param slider: сам слайдер
-        :return: значение в px, на которое нужно сдвинуть слайдер
-        340 - ширина слайдера
-        """
-
-        width = 340
-        position = slider.get_attribute('style')
-        length = len(position)
-        position = float(position[6:length - 2])
-        return width * (hour / 24 - position / 100)
-
-    def wait_load(self):
-        WebDriverWait(self.driver, 5).until(EC.element_to_be_clickable((By.CLASS_NAME, 'dataViewer__frames')))
 
     def setUp(self):
         """
@@ -39,42 +21,13 @@ class FirstTestCase(unittest.TestCase):
 
         self.driver = webdriver.Firefox()
         self.driver.implicitly_wait(20)
-        self.driver.get('2gis.ru')
-
-        action = ActionChains(self.driver)
-        action.send_keys('компьютеры')
-        action.perform()
-
-        self.driver.find_element_by_class_name('searchBar__submit').click()
-        self.driver.find_element_by_xpath("//div[@class='filters__primaryExtended']").click()
+        self.page = Page(self.driver)
+        self.page.open("http://2gis.ru")
+        self.page.search_bar.search('компьютеры')
+        self.page.search_result.filters_open()
 
     def tearDown(self):
-        self.driver.quit()
-
-    def check_filter(self, filter_xpath):
-        filt = self.driver.find_element_by_xpath(filter_xpath)
-        if not filt.is_selected():
-            filt.click()
-
-    def test_filter(self, asserttext):
-        self.wait_load()
-
-        text = self.driver.find_element_by_class_name('mixedResults__header').text
-        self.assertEqual(asserttext, text)
-
-    def select_day(self, day):
-        self.wait_load()
-
-        day_xpath = "//div[@class='radiogroup _week']/label[" + str(day) + "]"
-        self.driver.find_element_by_xpath(day_xpath).click()
-
-    def select_hour(self, hour):
-        self.wait_load()
-
-        action = ActionChains(self.driver)
-        rader_runner = self.driver.find_element_by_class_name('filters__raderRunner')
-        rader_runner_in = self.driver.find_element_by_class_name('filters__raderRunnerIn')
-        action.drag_and_drop_by_offset(rader_runner_in, self.slide_move(hour, rader_runner), 0).perform()
+        self.driver.close()
 
     def test_has_site(self):
         """
@@ -83,10 +36,13 @@ class FirstTestCase(unittest.TestCase):
         Expected result:
         отфильтровываются организации не имеющие сайта(их становится меньше)
         """
-        checkbox = "//label[@class='checkbox' and @title='Есть сайт']"
-        asserttext = '622 организации'
-        self.check_filter(checkbox)
-        self.test_filter(asserttext)
+        filter_name = 'has_site'
+        asserttext = '621 организация'
+        self.page.filters_scroller.check_filter(filter_name)
+
+        self.page.search_result.wait_load()
+        text = self.page.search_result.counter()
+        self.assertEqual(asserttext, text)
 
     def test_has_photos(self):
         """
@@ -96,10 +52,13 @@ class FirstTestCase(unittest.TestCase):
         отфильтровываются организации не имеющие фото(их становится меньше)
         """
 
-        checkbox = "//label[@class='checkbox' and @title='Есть фото']"
+        filter_name = 'has_photo'
         asserttext = '102 организации'
-        self.check_filter(checkbox)
-        self.test_filter(asserttext)
+        self.page.filters_scroller.check_filter(filter_name)
+
+        self.page.search_result.wait_load()
+        text = self.page.search_result.counter()
+        self.assertEqual(asserttext, text)
 
     def test_has_card(self):
         """
@@ -109,10 +68,13 @@ class FirstTestCase(unittest.TestCase):
         отфильтровываются организации не имеющие расчета по картам(их становится меньше)
         """
 
-        checkbox = "//label[@class='checkbox' and @title='Расчет по картам']"
+        filter_name = 'has_card'
         asserttext = '263 организации'
-        self.check_filter(checkbox)
-        self.test_filter(asserttext)
+        self.page.filters_scroller.check_filter(filter_name)
+
+        self.page.search_result.wait_load()
+        text = self.page.search_result.counter()
+        self.assertEqual(asserttext, text)
 
     def test_work_all_time(self):
         """
@@ -122,10 +84,13 @@ class FirstTestCase(unittest.TestCase):
         отфильтровываются организации не работающие круглосуточно(их становится меньше)
         """
 
-        checkbox = "//label[@class='radiogroup__label'][2]"
+        filter_name = 'work_all_time'
         asserttext = '13 организаций'
-        self.check_filter(checkbox)
-        self.test_filter(asserttext)
+        self.page.filters_scroller.check_filter(filter_name)
+
+        self.page.search_result.wait_load()
+        text = self.page.search_result.counter()
+        self.assertEqual(asserttext, text)
 
     def test_work_select_time(self):
         """
@@ -137,14 +102,22 @@ class FirstTestCase(unittest.TestCase):
         отфильтровываются организации не работающие в данное время(их становится меньше)
         """
 
-        checkbox = "//label[@class='radiogroup__label'][3]"
+        filter_name = 'work_select_time'
         asserttext = '193 организации'
-        week = 2
+        day = 'Вт'
         hour = 20
-        self.check_filter(checkbox)
-        self.select_day(week)
-        self.select_hour(hour)
-        self.test_filter(asserttext)
+        self.page.filters_scroller.check_filter(filter_name)
 
-if __name__ == "__main__":
+        self.page.search_result.wait_load()
+        self.page.filters_scroller.select_day(day)
+
+        self.page.search_result.wait_load()
+        self.page.filters_scroller.select_hour(hour)
+
+        self.page.search_result.wait_load()
+        text = self.page.search_result.counter()
+        self.assertEqual(asserttext, text)
+
+
+if __name__ == '__main__':
     unittest.main()
