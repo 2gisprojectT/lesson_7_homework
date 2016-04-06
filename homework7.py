1ec8b8ee831a99df9ca143eddbdc00d34a3ce88f
Маша <Gitaristka3@yandex.ru>
2016-04-06 23:50:03 +0600

    add_description_test-case

--- homework7.py
+++ homework7.py
@@ -1,9 +1,21 @@
+'''
+Тест-кейс "Ввод текста, не являющегося Email-адресом"
+Шаги:
+  1. Зайти на сайт "mail.google.com"
+  2. Нажать "Нужна помощь?"
+  3. Выбрать вариант "При входе в систему возникают другие проблемы"
+  4. В открывшемся текстовом поле набрать любой текст, не являющийся Email-адресом
+     (В данном тест-кейсе "This is not Email")
+Ожидание:
+  Вывод сообщения об ошибке "Введите действительный адрес электронной почты." в браузере,
+  программа выдает сообщение "Тест завершен успешно"
+'''
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
 
 driver = webdriver.Firefox()
-driver.get("http://mail.google.com/mail/#inbox")
+driver.get("http://mail.google.com")
 elem = driver.find_element_by_class_name("need-help")
 
 elem.send_keys(Keys.RETURN)
