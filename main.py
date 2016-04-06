3726f9f6cce53ecee6da8fc29264fe6d75534799
Vadim Potapov <doctorvra4@gmail.com>
2016-04-06 22:47:35 +0600

    Написал автотест

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
+import time
+
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 driver = webdriver.Firefox()
-driver.get("http://www.python.org")
-assert "Python" in driver.title
-elem = driver.find_element_by_name("q")
-elem.send_keys("pycon")
+driver.get("https://mail.google.com/")
+elem = driver.find_element_by_name("Email")
+elem.send_keys("doctorvra4@gmail.com")
 elem.send_keys(Keys.RETURN)
-assert "No results found." not in driver.page_source
-driver.close()
+time.sleep(1)
+elem1 = driver.find_element_by_name("Passwd")
+elem1.send_keys("123")
+elem1.send_keys(Keys.RETURN)
+time.sleep(1)
+assert "Указан неправильный адрес или пароль" in driver.page_source
+print("Тест пройден")
+driver.quit()
