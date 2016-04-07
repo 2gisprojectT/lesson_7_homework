b82023ad7f0de5371203848ff44caa44d9b6eb36
Volkova Natalya <nata.volkowa@yandex.ru>
2016-04-07 20:09:35 +0600

    Update Selenium2gisTest.py
    убрала лишний wait

--- Selenium2gisTest.py
+++ Selenium2gisTest.py
@@ -26,7 +26,6 @@ class SeleniumTest2GIS(TestCase):
         search_bar = self.driver.find_element_by_class_name("suggest__input")
         search_bar.send_keys("версаль, торгово-офисный центр")
         search_bar.submit()
-        self.driver.implicitly_wait(5)
         search_mini_card = self.driver.find_element_by_class_name("miniCard__headerTitleLink")
         search_link = search_mini_card.get_attribute("href")
         self.assertEqual("http://2gis.ru/novosibirsk/firm/141265769436866?queryState=center%2F82.891224%2C54.98376%2Fzoom%2F17", search_link)
