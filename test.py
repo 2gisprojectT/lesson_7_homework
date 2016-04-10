b68c41f60e438f1d974c43c7c43df0eaafbd439f
Tima <temirlan-nsk@mail.ru>
2016-04-10 18:59:23 +0600

    Изменения с учетом замечаний №1

--- test.py
+++ test.py
@@ -1,44 +1,40 @@
 from unittest import TestCase
 from selenium import webdriver
 from selenium import common
-from pdb import set_trace
+from pdb import set_trace2
 
 
 class TestOneTwoTrip(TestCase):
-    """ Тест кейс:
-
-    Проверка на кол-во билетов в наборе:
+    """ 
     Предусловия:
-        Заходим на сайт 'https://www.onetwotrip.com/ru/'
-    Шаги:
-        1. Кликаем на кнопку 'Наборный'
-        2. Вводим в поле 'Откуда' название любого города на русском
-        3. Должна появиться еще одна группа полей для набора информации по
-        следующему билету.
-        4. Делаем 2 и 3 шаг, пока новые группы полей не перестанут появляться.
-        5. Подсчитываем кол-во возможных искомых билетов в наборе.
-    Ожидаемый рез-ат:
-       Это кол-во должно быть равно норме.
-
-    """
-
-    n = 4
-
+	    Заходим на сайт 'https://www.onetwotrip.com/ru/'"
+	"""
     def setUp(self):
         driver = webdriver.Firefox()
         driver.get('https://www.onetwotrip.com/ru/')
         driver.implicitly_wait(10)
         self.driver = driver
 
-    def test_setOrd(self):
-        i, driver = 0, self.driver
+    """ 
+    Проверка на кол-во билетов в наборе:
+    Шаги:
+        1. Кликаем на кнопку 'Наборный'
+        2. Вводим в поле 'Откуда' название любого города на русском, 
+        3. Должна появиться еще одна группа полей для набора информации по
+        следующему билету, переходим туда.
+        4. Делаем 2 и 3 шаг, пока новые группы полей не перестанут появляться.
+        5. Подсчитываем кол-во возможных искомых билетов в наборе.
+    Ожидаемый рез-ат:
+       Это кол-во должно быть равно определенной норме.
+    """
+    def test_setOrd(self):	
+        maxOrd, n, driver = 10, 4, self.driver
         element = driver.find_element_by_css_selector('li.multiway')
         element.click()
-        while 1:
+        for i in range(0, maxOrd)
             try:
                 element = driver.find_element_by_id('from' + str(i))
                 element.send_keys('Амстерд')
-                i += 1
             except common.exceptions.NoSuchElementException:
                 self.assertEqual(self.n, i, "Кол-во возможных искомых билетов в наборе не равно норме")
                 break
@@ -47,4 +43,5 @@ class TestOneTwoTrip(TestCase):
     def tearDown(self):
         self.driver.quit()
 
-
+if __name__ == '__main__':
+	unittest.main()
