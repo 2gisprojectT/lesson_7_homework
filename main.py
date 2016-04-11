76c3e0a450d4f4dd508228cc14473cb3d44e0fb8
alexey <alexld45@mail.ru>
2016-04-12 01:56:01 +0600

    изменён assert и заголовок тест-кейса,добавлен teardown

--- main.py
+++ main.py
@@ -6,23 +6,25 @@ from selenium.webdriver.common.keys import Keys
 
 
 class OttripTest(TestCase):
-    """
-    Тест-кейс "Ввод несуществующего email в форме "забыли пароль""
-     Шаги:
-     1. Зайти на сайт "www.onetwotrip.com"
-     2. Нажать "Личный кабинет"
-     3. В открывшемся окне нажимаем "забыли пароль"
-     4. В открывшемся окне ввести несуществующий email
- Ожидание:
-    Вывод сообщения об ошибке "Пользователя с таким email не существует" в браузере.
-    """
-
     def setUp(self):
+        """
+        Предусловие: зайти на сайт www.onetwotrip.com
+        """
         self.driver = webdriver.Firefox()
         self.driver.maximize_window()
         self.driver.get("http://www.onetwotrip.com/ru")
 
     def test(self):
+        """
+        Тест-кейс "Проверка вывода сообщения об ошибке при вводе несуществующего email в форме "забыли пароль""
+        Шаги:
+        1. Зайти на сайт "www.onetwotrip.com"
+        2. Нажать "Личный кабинет"
+        3. В открывшемся окне нажимаем "забыли пароль"
+        4. В открывшемся окне ввести несуществующий email
+        Ожидание:
+        Вывод сообщения об ошибке "Пользователя с таким email не существует" в браузере.
+        """
         self.driver.find_element_by_class_name("enter").click()
         self.driver.find_element_by_class_name("getNewPas").click()
         self.driver.find_element_by_id("input_remind_email").send_keys("llllll@mail.ru")
@@ -30,7 +32,9 @@ class OttripTest(TestCase):
             "table.layout:nth-child(2) > tbody:nth-child(1) > tr:nth-child(1) > td:nth-child(1) > button:nth-child(1)").click()
         self.driver.find_element_by_id("RemindAuth")
         error = self.driver.find_element_by_class_name("Error")
-        self.assertTrue("Пользователя с таким email не существует" in error.text)
+        self.assertIn("Пользователя с таким email не существует" in error.text)
+
+    def TearDown(self):
         self.driver.quit()
 
 
